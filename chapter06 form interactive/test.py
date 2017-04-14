e49f174300b120330e07c1fd38ac62f5a33f3516
lpbirdueng <lupeng0924@sina.com>
2017-04-14 16:51:05 +0800

    Work-add chapter06

---
+++ chapter06 form interactive/test.py
@@ -0,0 +1,10 @@
+import lxml.html
+
+
+def parse_form(html):
+    tree = lxml.html.fromstring(html)
+    data = {}
+    for e in tree.cssselect('form input'):
+        if e.get('name'):
+            data[e.get('name')] = e.get('value')
+    return data
