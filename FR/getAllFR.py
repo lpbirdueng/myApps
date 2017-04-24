17aa020435febec322d5e5a7c697ab15ee2e3e5d
lpbirdueng <lupeng0924@sina.com>
2017-04-24 17:03:29 +0800

    work

--- FR/getAllFR.py
+++ FR/getAllFR.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
     db = mongodb_utility.connect_db(db_name="list_company")
     sz_df = mongodb_utility.export2df(database=db,collection="sz_stock")
     sh_df = mongodb_utility.export2df(database=db,collection="sh_stock")
-    sz_list = list(sz_df[Series(sz_df['A股上市日期']).notnull()]["_id"])
+    df = sz_df[Series(sz_df['A股上市日期']).notnull()][["_id", "A股上市日期"]]
+    df["A股上市日期"] = df["A股上市日期"].apply(lambda x: str(x)[0:4])
+    #sz_list = list(sz_df[[Series(sz_df['A股上市日期']).notnull()]["_id","A股上市日期"]])
     sh_list = list(sh_df['_id'])
     szb_list = list(sz_df[Series(sz_df['B股上市日期']).notnull()]['B股代码'])
-    #print(sz_list)
+    print(df.values.tolist())
     url = 'http://www.cninfo.com.cn/cninfo-new/data/download'
     #frDownloader.get_fixed_year(url=url,stock_list=sz_list,max_year='2017')
     #frDownloader.getFR(url=url,stock_list=stockCode.szStockList)
