64d69abea405c95ab36ac588cc8cf6dd63d6d30d
lpbirdueng <lupeng0924@sina.com>
2017-05-09 16:55:06 +0800

    Work - Update

--- ChinaA/AIndexData.py
+++ ChinaA/AIndexData.py
@@ -12,10 +12,10 @@ from datetime import timedelta
 from datetime import date
 
 
-def getIndexData(url_prefix, days=-1, start_date = date.today(), path=None, max_errors=20):
+def getIndexData(url_prefix, days=-1, start_date=date.today(), path=None, max_errors=20):
     D = Downloader()
     single_day = timedelta(days=1)
-    #current_date = date.today() - single_day
+    # current_date = date.today() - single_day
     current_date = start_date - single_day
     if days > 0:
         for day in range(days):
@@ -27,8 +27,8 @@ def getIndexData(url_prefix, days=-1, start_date = date.today(), path=None, max_
             if path is None:
                 fullfilename = date_str + '.zip'
             else:
-                fullfilename = path + "\\"+ date_str + '.zip'
-            D.saveZipToLocal(url,filename=fullfilename,extract=True)
+                fullfilename = path + "\\" + date_str + '.zip'
+            D.saveZipToLocal(url, filename=fullfilename, extract=True)
             current_date = current_date - single_day
     else:
         num_errors = 0
@@ -41,20 +41,21 @@ def getIndexData(url_prefix, days=-1, start_date = date.today(), path=None, max_
             if path is None:
                 fullfilename = date_str + '.zip'
             else:
-                fullfilename = path + "\\"+ date_str + '.zip'
-            if D.saveZipToLocal(url,filename=fullfilename,extract=True):
+                fullfilename = path + "\\" + date_str + '.zip'
+            if D.saveZipToLocal(url, filename=fullfilename, extract=True):
                 num_errors = 0
             else:
                 num_errors = num_errors + 1
             if num_errors >= max_errors:
                 break
             current_date = current_date - single_day
-            
+
+
 if __name__ == '__main__':
-    getIndexData(url_prefix="http://115.29.204.48/syl/",days=3)
-    getIndexData(url_prefix="http://115.29.204.48/syl/csi",days=3)
-    getIndexData(url_prefix="http://115.29.204.48/syl/bk",days=3)
-    #start_date = date(2016,8,9)
-    #getIndexData(url_prefix="http://115.29.204.48/syl/",start_date = start_date)
-    #getIndexData(url_prefix="http://115.29.204.48/syl/csi")
-    #getIndexData(url_prefix="http://115.29.204.48/syl/bk",start_date = start_date)   
+    getIndexData(url_prefix="http://115.29.204.48/syl/", days=3)
+    getIndexData(url_prefix="http://115.29.204.48/syl/csi", days=3)
+    getIndexData(url_prefix="http://115.29.204.48/syl/bk", days=3)
+    # start_date = date(2016,8,9)
+    # getIndexData(url_prefix="http://115.29.204.48/syl/",start_date = start_date)
+    # getIndexData(url_prefix="http://115.29.204.48/syl/csi")
+    # getIndexData(url_prefix="http://115.29.204.48/syl/bk",start_date = start_date)
---
+++ scs/scstest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from sinastorage.bucket import SCSBucket
+import sinastorage
+from zipfile import ZipFile
+from io import BytesIO
+
+
+with ZipFile('test.zip', 'r') as zf:
+    sinastorage.setDefaultAppInfo('16wn9n74cnZJFjffXU3K', '50706013e9a806c252aafb03ad2a3b51174e36ad')
+    s = SCSBucket('saepcs', secure=False)  # secure=True 采用https访问方式
+    scsResponse = s.putFile('stock/test.zip', 'test.zip')
+
