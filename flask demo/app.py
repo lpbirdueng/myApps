302424976425dcd3d09a9ef65bc6f16895e3e6e5
lpbirdueng <lupeng0924@sina.com>
2017-05-14 15:41:50 +0800

    work -update

--- flask demo/app.py
+++ flask demo/app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 
 app = Flask(__name__)
@@ -7,7 +7,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return '<h1>Home</h1>'
+    return render_template('home.html')
+
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+@app.route('/signin',methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'password':
+        return render_template('signin-ok.html',username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
 
 
 if __name__ == '__main__':
